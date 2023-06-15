c4f9a2c5521f7b103c14c9ccd352535590f7f08f
jesusre890 <jesusre890@gmail.com>
2023-06-15 13:10:18 -0300

    sequelize

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,40 +1,28 @@
-const express=require("express");
-const morgan=require('morgan')
-const router = require('./routes/index.js')
-const server = express();
 const PORT=3001;
+const server=require('./app.js');
+const {conn}=require("./DB_connection");
+//const express=require('express') 
+//const server= express()
 
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
-server.use(express.json()) // midd
-server.use(morgan('dev'))  // midd
 
-//localhost:3001//rickandmorty/postFav
-server.use("/rickandmorty", router);
-
-server.listen(PORT, () => {
+server.listen(PORT, async () => {
+  await conn.sync({ force: true }),
   console.log("Server raised in port: " + PORT);
 });
 
-//const http = require("http");
-//const getCharById = require("./controllers/getCharById");
-//const PORT = 3001;
 
-//http
-//  .createServer((req, res) => {
-//    res.setHeader("Access-Control-Allow-Origin", "*");
 
-//    if (req.url.includes("/rickandmorty/character")) {
-//      const id = req.url.split("/").pop(); //string
-//      getCharById(res, id);
-//    }
-//  })
-//  .listen(PORT, "localhost");
+//server.use((req, res, next) => {
+//  res.header("Access-Control-Allow-Origin", "*");
+//  res.header("Access-Control-Allow-Credentials", "true");
+//  res.header(
+//    "Access-Control-Allow-Headers",
+//    "Origin, X-Requested-With, Content-Type, Accept"
+//  );
+//  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//  next();
+//});
+//server.use(express.json()); // midd
+////server.use(morgan('dev'))  // midd
+////localhost:3001//rickandmorty/postFav
+//server.use("/rickandmorty", router);
---
+++ Server/test/index.test.js
@@ -0,0 +1,7 @@
+//const app = require("../src/app");
+//const session = require("supertest");
+//const agent = session(app);
+
+//describe("Route: GET /rickandmorty/character/:id",() => {
+    
+//});
